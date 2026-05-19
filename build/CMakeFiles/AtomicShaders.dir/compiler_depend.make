bab7fe28cef288df303ddf90e9583422b1010e6a
k <ketsebaot.vibes@gmail.com>
2026-05-19 20:37:38 +0300

    remove build folder mistake push

--- build/CMakeFiles/AtomicShaders.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for AtomicShaders.
-# This may be replaced when dependencies are built.
--- build/_deps/sdl3-build/CMakeFiles/SDL3-static.dir/src/main/SDL_runapp.c.o.d
+++
@@ -1,5 +0,0 @@
-_deps/sdl3-build/CMakeFiles/SDL3-static.dir/src/main/SDL_runapp.c.o: \
- /home/k/Documents/atomic/build/_deps/sdl3-src/src/main/SDL_runapp.c \
- /usr/include/stdc-predef.h \
- /home/k/Documents/atomic/build/_deps/sdl3-src/src/SDL_internal.h \
- /home/k/Documents/atomic/build/_deps/sdl3-src/src/main/SDL_main_callbacks.h
